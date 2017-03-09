aa2df860045d4369368e23694acec7c045b9d23b
Joshua Martin <the.manuscriptmaster@gmail.com>
2017-03-09 14:47:09 -0600

    Form submissions saved to local storage

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 var Koa = require('koa');
 var router = require('./routes')
+var bodyParser = require('koa-bodyparser');
 
 var app = new Koa();
 
+app.use(bodyParser());
 app.use(router.routes());
 
 app.listen(3000);
