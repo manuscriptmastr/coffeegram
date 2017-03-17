9d57adac5f3c356682cd892596968de28508ebfc
Joshua Martin <the.manuscriptmaster@gmail.com>
2017-03-17 16:09:57 -0500

    Nybblr code review

--- routes.js
+++ routes.js
@@ -1,6 +1,5 @@
 const Router = require('koa-router');
 const { User, Coffeegram } = require('./database');
-const pug = require('pug');
 
 let router = Router();
 
@@ -15,7 +14,6 @@ router.get('/users/:username', async ctx => {
   var user = await User.findOne({ username: ctx.params.username });
   var coffeegrams = await Coffeegram.find({ userId: user.id });
   await ctx.render('usergrams', {
-    currentUser: ctx.state.currentUser,
     user,
     coffeegrams
   });
