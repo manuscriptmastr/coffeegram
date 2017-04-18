49f0f2a92249920cd1925d08af3114f20218c449
Joshua Martin <the.manuscriptmaster@gmail.com>
2017-04-18 16:52:41 -0500

    User can edit existing coffeegram

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,8 @@ const { clean, isBlank } = require('underscore.string');
 
 let router = Router();
 
+let hasOwnProperty = (obj, key) => Object.hasOwnProperty.call(obj, key);
+
 router.get('/', async ctx => {
   var coffeegrams = await Coffeegram.find({}).sort({ createdAt: 'desc' });
   await ctx.render('home', {
@@ -41,19 +43,40 @@ router.post('/coffeegrams', async ctx => {
     shop: form['shop']
   });
 
-  ctx.request.flash('success', "Your new Coffeegram is up!");
+  ctx.request.flash('success', "Your new Coffeegram is up");
 
   ctx.redirect(`/users/${ctx.state.currentUser.username}`);
 });
 
 router.get('/coffeegrams/:id', async ctx => {
   var coffeegram = await Coffeegram.findById(ctx.params.id);
-  if(!coffeegram) {
+  if (!coffeegram) {
     throw new NotFound();
   }
-  await ctx.render('edit', {
+  await ctx.render('gram', {
     coffeegram
   });
+});
+
+router.post('/coffeegrams/:id', async ctx => {
+  var coffeegram = await Coffeegram.findById(ctx.params.id);
+  if (coffeegram.userId.toString() !== ctx.state.currentUser.id) {
+    ctx.request.flash('error', "You're not allowed to edit someone else's coffeegram");
+    return ctx.redirect('back');
+  }
+
+  var { description, shop } = ctx.request.body;
+
+  if (hasOwnProperty(ctx.request.body, 'description') && !isBlank(description)) {
+    coffeegram.description = description;
+  }
+  if (hasOwnProperty(ctx.request.body, 'shop') && !isBlank(shop)) {
+    coffeegram.shop = shop;
+  }
+  await coffeegram.save();
+
+  ctx.request.flash('success', "Your new Coffeegram is updated");
+  ctx.redirect(`/coffeegrams/${coffeegram.id}`);
 });
 
 router.post('/users/:username', async ctx => {
@@ -69,7 +92,7 @@ router.post('/users/:username', async ctx => {
   var { bio } = ctx.request.body;
   bio = clean(bio);
   if (isBlank(bio)) {
-    ctx.request.flash('error', "Your bio looks empty!");
+    ctx.request.flash('error', "Your bio looks empty");
     return ctx.redirect('back');
   }
 
